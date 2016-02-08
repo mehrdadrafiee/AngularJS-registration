a9cc0748461ecfee52a354725e611de50fdbb1a0
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2016-02-07 21:18:00 -0800

    saved user info into firebase object

--- builds/angular-registration/js/services/authentication.js
+++ builds/angular-registration/js/services/authentication.js
@@ -13,6 +13,16 @@ myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', 'FIREBASE_URL',
           email: user.email,
           password: user.password
         }).then(function(regUser) {
+
+          var regRef = new Firebase(FIREBASE_URL + 'users')
+          .child(regUser.uid).set({
+            date: Firebase.ServerValue.TIMESTAMP,
+            regUser: regUser.uid,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            email: user.email
+          });//User Info
+
           $rootScope.message = "Hi " + user.firstName + "! Thanks for registering";
         }).catch(function(error) {
           $rootScope.message = error.message;
