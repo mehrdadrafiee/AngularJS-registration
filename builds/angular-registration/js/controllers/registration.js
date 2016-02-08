9e5cd452804d85b101d1df2a5b621dc76ea09bb9
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2016-02-07 21:00:07 -0800

    added authentication service(factory)

--- builds/angular-registration/js/controllers/registration.js
+++ builds/angular-registration/js/controllers/registration.js
@@ -1,23 +1,13 @@
 myApp.controller('RegistrationController',
-  ['$scope', '$firebaseAuth', 'FIREBASE_URL', function($scope, $firebaseAuth, FIREBASE_URL) {
-
-  var ref = new Firebase(FIREBASE_URL);
-  var auth = $firebaseAuth(ref);
+  ['$scope', 'Authentication', function($scope, Authentication) {
 
 
   $scope.login = function() {
-    $scope.message = "Welcome " + $scope.user.email + "!";
+    Authentication.login($scope.user);
   };//Login
 
   $scope.register = function() {
-    auth.$createUser({
-      email: $scope.user.email,
-      password: $scope.user.password
-    }).then(function(regUser) {
-      $scope.message = "Hey " + $scope.user.firstName + "! Thanks for registering.";
-    }).catch(function(error) {
-      $scope.message = error.message;
-    });//Create User
+    Authentication.register($scope.user);
   };//Register
 
 }]);//Controller
