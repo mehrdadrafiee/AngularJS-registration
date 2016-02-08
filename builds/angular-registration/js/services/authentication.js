aef2be6796a52f77d815e7c34dab7a1f5578706c
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2016-02-07 21:26:50 -0800

    redirecting logged in user to success page

--- builds/angular-registration/js/services/authentication.js
+++ builds/angular-registration/js/services/authentication.js
@@ -1,13 +1,21 @@
-myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', 'FIREBASE_URL',
-  function($rootScope, $firebaseAuth, FIREBASE_URL) {
+myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', '$location', 'FIREBASE_URL',
+  function($rootScope, $firebaseAuth, $location, FIREBASE_URL) {
 
     var ref = new Firebase(FIREBASE_URL);
     var auth = $firebaseAuth(ref);
 
     return {
       login: function(user) {
-        $rootScope.message = "Welcome " + user.email;
+        auth.$authWithPassword({
+          email: user.email,
+          password: user.password
+        }).then(function(regUser) {
+          $location.path('/success');
+        }).catch(function(error) {
+          $rootScope.message = error.message;
+        });
       },
+
       register: function(user) {
         auth.$createUser({
           email: user.email,
