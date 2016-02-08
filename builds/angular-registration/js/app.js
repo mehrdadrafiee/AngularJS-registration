2af75406545944d8bec1047e1a1221d2dc03314b
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2016-02-07 22:11:34 -0800

    added routeChangeError to prevent loading a page without auth

--- builds/angular-registration/js/app.js
+++ builds/angular-registration/js/app.js
@@ -2,6 +2,18 @@ var myApp = angular.module('myApp',
   ['ngRoute', 'firebase'])
   .constant('FIREBASE_URL', 'https://angularjs-register.firebaseio.com/');
 
+// Run
+myApp.run(['$rootScope', '$location', function($rootScope, $location) {
+  //event info
+  $rootScope.$on('$routeChangeError', function(event, next, previous, error) {
+    if(error=='AUTH_REQUIRED') {
+      //AUTH REQUIRED
+      $rootScope.message = 'Sorry, you must log in to access that page.';
+      $location.path('/login');
+    }
+  });
+}]);
+
 myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
     when('/login', {
@@ -14,7 +26,12 @@ myApp.config(['$routeProvider', function($routeProvider) {
     }).
     when('/success', {
       templateUrl: 'views/success.html',
-      controller: 'SuccessController'
+      controller: 'SuccessController',
+      resolve: {
+        currentAuth: function(Authentication) {
+          return Authentication.requireAuth();
+        }
+      }
     }).
     otherwise({
       redirectTo: '/login'
--- builds/angular-registration/js/services/authentication.js
+++ builds/angular-registration/js/services/authentication.js
@@ -1,55 +1,66 @@
+//Factory
 myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', '$firebaseObject', '$location', 'FIREBASE_URL',
   function($rootScope, $firebaseAuth, $firebaseObject, $location, FIREBASE_URL) {
 
-    var ref = new Firebase(FIREBASE_URL);
-    var auth = $firebaseAuth(ref);
+  var ref = new Firebase(FIREBASE_URL);
+  var auth = $firebaseAuth(ref);
 
-    auth.$onAuth(function(authUser) {
-      if (authUser) {
-        var userRef = new Firebase(FIREBASE_URL + 'users/' + authUser.uid);
-        var userObj = $firebaseObject(userRef);
-        $rootScope.currentUser = userObj;
-      } else {
-        $rootScope.currentUser = '';
-      }
-    });
+  auth.$onAuth(function(authUser) {
+    if (authUser) {
+      var userRef = new Firebase(FIREBASE_URL + 'users/' + authUser.uid);
+      var userObj = $firebaseObject(userRef);
+      $rootScope.currentUser = userObj;
+    } else {
+      $rootScope.currentUser = '';
+    }
+  });
 
-    return {
-      login: function(user) {
-        auth.$authWithPassword({
-          email: user.email,
-          password: user.password
-        }).then(function(regUser) {
-          $location.path('/success');
-        }).catch(function(error) {
-          $rootScope.message = error.message;
+  return {
+    //Login
+    login: function(user) {
+      auth.$authWithPassword({
+        email: user.email,
+        password: user.password
+      }).then(function(regUser) {
+        $location.path('/success');
+      }).catch(function(error) {
+        $rootScope.message = error.message;
+      });
+    },
+
+    //Logout
+    logout: function() {
+      return auth.$unauth();
+    },
+
+    //Reuire Authentication
+    requireAuth: function() {
+      return auth.$requireAuth();
+    },
+
+    //Register
+    register: function(user) {
+      //createUser
+      auth.$createUser({
+        email: user.email,
+        password: user.password
+      }).then(function(regUser) {
+
+        //User Info
+        var regRef = new Firebase(FIREBASE_URL + 'users')
+        .child(regUser.uid).set({
+          date: Firebase.ServerValue.TIMESTAMP,
+          regUser: regUser.uid,
+          firstName: user.firstName,
+          lastName: user.lastName,
+          email: user.email
         });
-      },
 
-      logout: function() {
-        return auth.$unauth();
-      },
+        $rootScope.message = "Hi " + user.firstName + "! Thanks for registering";
+      }).catch(function(error) {
+        $rootScope.message = error.message;
+      });
+    }
+  };
 
-      register: function(user) {
-        auth.$createUser({
-          email: user.email,
-          password: user.password
-        }).then(function(regUser) {
-
-          var regRef = new Firebase(FIREBASE_URL + 'users')
-          .child(regUser.uid).set({
-            date: Firebase.ServerValue.TIMESTAMP,
-            regUser: regUser.uid,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            email: user.email
-          });//User Info
-
-          $rootScope.message = "Hi " + user.firstName + "! Thanks for registering";
-        }).catch(function(error) {
-          $rootScope.message = error.message;
-        }); //createUser
-      } //Register
-    };
-
-  }]); //Factory
+}]);
