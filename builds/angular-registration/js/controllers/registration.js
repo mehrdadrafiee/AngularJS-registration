bd8bb0e29d379cfc66800b678bd172dd5b2a8a47
Mehrdad Rafiee <rafiee.mehrdad@gmail.com>
2016-02-07 19:48:03 -0800

    validate login and register forms

--- builds/angular-registration/js/controllers/registration.js
+++ builds/angular-registration/js/controllers/registration.js
@@ -1,3 +1,10 @@
 myApp.controller('RegistrationController', ['$scope', function($scope) {
-  $scope.message = "Welcome to my app";
+
+  $scope.login = function() {
+    $scope.message = "Welcome " + $scope.user.email + "!";
+  };
+
+  $scope.register = function() {
+    $scope.message = "Hey " + $scope.user.firstName + "! You just registered.";
+  }
 }]);
